ad5e93e5018a5959f353c89ea134b6bcdb96f7f6
SineMag <snenhlanhlamagubane99@gmail.com>
2025-08-06 12:54:19 +0200

    Created a text input with Add button.

--- App.js
+++ App.js
@@ -1,3 +1,25 @@
 // All your DOM manipulation must happen here.
 // You will create and inject all elements into <main id="root"> using JavaScript only.
 
+let root = document.getElementById("root");
+document.body.appendChild(root);
+
+// add text input to innerHTML
+let input = document.createElement("input");
+input.type = "text";
+input.placeholder = "Add a new task...";
+root.appendChild(input);
+
+
+
+// add the "Add" button
+let button = document.createElement("button");
+button.textContent = "Add Task";
+root.appendChild(button);
+
+//making my tasks to appear in an unordered list
+let taskLists = document.createElement("ul.list").addClass("task-list");
+taskLists.textContent = "Tasks:";
+root.appendTo("ul.task-list");
+
+
